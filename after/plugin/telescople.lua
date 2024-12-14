0270697c0a824d7e93a39fe040a7c1343930dfbd
lecterkn <lecterkn@proton.me>
2024-12-15 02:09:20 +0900

    set 'pf' to git_files

--- after/plugin/telescople.lua
+++ after/plugin/telescople.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 
 -- mapping
 -- find files
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 -- grep
 vim.keymap.set('n', '<leader>ss', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
