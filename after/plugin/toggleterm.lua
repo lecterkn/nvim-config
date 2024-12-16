0ed72fb52cbae8414f6e59e8e33c5d9656e3f0a1
lecterkn <lecterkn@proton.me>
2024-12-16 21:35:55 +0900

    updated terminal settings

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,12 +1,19 @@
 require("toggleterm").setup {
-    shade_terminals = false,
-    winbar = {
-        enabled = false,
-        name_formatter = function(term)
-            return term.name
-        end
-    },
 }
 
 -- mapping
-vim.keymap.set("n", "<leader>h", vim.cmd.ToggleTerm)
+vim.keymap.set("n", "<leader>h", ":ToggleTerm direction=horizontal<CR>")
+vim.keymap.set("n", "<leader>f", ":ToggleTerm direction=float<CR>")
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
